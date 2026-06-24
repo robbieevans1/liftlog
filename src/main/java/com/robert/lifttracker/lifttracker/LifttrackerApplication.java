c8cec354eed2daa6271de23e227bf7ec785e1ed5
robcabobb <robcabobb@gmail.com>
2026-06-24 16:43:13 -0400

    added folders

---
+++ src/main/java/com/robert/lifttracker/lifttracker/LifttrackerApplication.java
@@ -0,0 +1,13 @@
+package com.robert.lifttracker.lifttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifttrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifttrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/robert/lifttracker/lifttracker/LifttrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robert.lifttracker.lifttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifttrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
